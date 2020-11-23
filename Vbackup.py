26fe6461e25fd6ca3527996de384a6609fd578e5
VaporeonMega <jackva24@sssas.org>
2020-11-23 13:50:12 -0500

    Made admin commands admin only
    • Basically I restricted them to you and I

--- Vbackup.py
+++ Vbackup.py
@@ -4,13 +4,17 @@ from discord.ext import commands
 bot = commands.Bot(command_prefix="!")
 bot.remove_command("help")
 
+adminperms = [395534835717898252,684836025788530718] #VaporeonMega and Invalid-User Cheese
+
 @bot.command(pass_context=True)
 async def kick(ctx, member: discord.Member):
-    await member.kick()
+    if int(ctx.author.id) in adminperms:
+        await member.kick()
 
 @bot.command(pass_context=True)
 async def ban(ctx, member: discord.Member):
-    await member.ban()
+    if int(ctx.author.id) in adminperms:
+        await member.ban()
 
 @bot.command(pass_context=True,aliases=["randomnum","randnum"])
 async def randomnumber(ctx, num1: int, num2: int):
